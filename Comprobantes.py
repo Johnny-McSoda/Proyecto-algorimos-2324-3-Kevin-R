e7547095e5e91f9ec779d59cb9f4363122bdc763
KevinRodriguez08 <168605054+Johnny-McSoda@users.noreply.github.com>
2024-07-02 08:18:30 -0400

    Update Comprobantes.py

--- Comprobantes.py
+++ Comprobantes.py
@@ -1,23 +1,20 @@
 import secrets
 
-dick = {}
+foo = {}
 def generador(seat):
     code = secrets.token_hex(5)
-    dick.update({seat:code})
+    foo.update({seat:code})
     print (f'({code})')
 
 
 def comprobador(seat,code):
-    k = dick[seat] 
+    k = foo[seat] 
     if code == k:
-        dick.pop(seat)
+        foo.pop(seat)
         print( f'El id del ticket ({code}) es valido')
     else:
         print(f'El id de su ticket es invalido o ya fue utilizado')
     
-generador('5A')
-x = input('Metala ')
-comprobador('5A',x.lower())
 
     
     
